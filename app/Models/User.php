a200b748b241997986ab8c070e20538b821a2fbe
Andri Sugilar <andrisglr1507@gmail.com>
2026-08-12 11:11:36 +0700

    prepare live server

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,8 +12,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
-    protected $table = 'user';
+    protected $table = 'users';
 
     protected $fillable = [
         'user_id',
